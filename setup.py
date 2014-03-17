0fd5bc858b78c7f14d315ed4bae924cda9b04f0c
Ronan Lamy <Ronan.Lamy@normalesup.org>
2014-03-17 03:01:12 +0000

    Add basic infrastructure: package, setup.py, tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mana',
+      version='0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      keywords='matrix matrices',
+      packages=['mana'],
+      )
