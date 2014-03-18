0a2d43674c321883dd75952061b32f95c84bacc0
Ronan Lamy <Ronan.Lamy@normalesup.org>
2014-03-18 02:21:45 +0000

    implement scalar * matrix

--- mana/core.py
+++ mana/core.py
@@ -7,4 +7,15 @@ class Matrix(object):
         self.array = arr
 
     def __mul__(self, other):
-        return Matrix(np.dot(self.array, other.array))
+        if isinstance(other, Matrix):
+            return Matrix(np.dot(self.array, other.array))
+        elif np.isscalar(other):
+            return Matrix(self.array * other)
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        if np.isscalar(other):
+            return Matrix(other * self.array)
+        else:
+            return NotImplemented
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,5 +4,9 @@ from mana import Matrix
 def test_basic():
     arr = np.asarray([[0, 1], [1, 0]])
     M = Matrix(arr)
-    arr2 = (M*M).array
+    arr2 = (M * M).array
     assert np.all(arr2 == np.asarray([[1, 0], [0, 1]]))
+    N1 = 2 * M
+    N2 = M * 2
+    assert np.all(N1.array == np.asarray([[0, 2], [2, 0]]))
+    assert np.all(N2.array == np.asarray([[0, 2], [2, 0]]))
